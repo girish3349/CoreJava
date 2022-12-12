0144e5abd3a045f1e7e668061bd0a95d54411817
Girish Bellamkonda <girish3349@gmail.com>
2022-12-11 19:37:42 -0500

    Added First class file

---
+++ src/com/DataTypes.java
@@ -0,0 +1,17 @@
+package com;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Get sizes of primitive data types
+
+		System.out.println("Size of Byte in Bytes:  " + Byte.SIZE / 8);
+		System.out.println("Size of short in Bytes:  " + Short.SIZE / 8);
+		System.out.println("Size of int in Bytes:  " + Integer.SIZE / 8);
+		System.out.println("Size of long in Bytes:  " + Long.SIZE / 8);
+		System.out.println("Size of float in Bytes:  " + Float.SIZE / 8);
+		System.out.println("Size of double in Bytes:  " + Double.SIZE / 8);
+
+	}
+
+}
